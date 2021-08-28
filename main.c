0945716696ec446e882bb6fb44a76480ef490d91
shahbaz ali <shahbaz_ali@outlook.in>
2021-08-28 21:59:26 +0530

    array init and input done

--- main.c
+++ main.c
@@ -1,7 +1,37 @@
 # include <stdio.h>
 
-int main(int argc, char *argv[])
+void scanA(int arr[], int size)
 {
-    printf("%s", argv[1]);
-    return 0;
+    for (int i = 0; i < size; i++)                          // taking input in the array
+    {
+        printf("Enter Element for position %d : ", i);
+        scanf("%d", &arr[i]);
+    }
+}
+
+void printA(int arr[], int size)
+{
+    printf("[ %d", arr[0]);
+    for (int i = 1; i < size; i++)                          
+    {
+        printf(", %d", arr[i]);
+    }
+    printf(" ] \n");
+    arr[0] = 42;
+}
+
+int main(int argc, char *argv[])                            // Driver code
+{
+    int size;
+    printf("Enter the size of an array : ");                // prompt
+    scanf("%d", &size);
+
+    int arr[size];                                          // creating array of required size
+    
+    scanA(arr, size);
+    printf("Enter elements of your array__ \n");
+    
+
+    printA(arr, size);
+
 }

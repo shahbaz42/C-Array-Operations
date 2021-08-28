a3ddcb782715c7a15f710b2845bea0046c478e91
shahbaz ali <shahbaz_ali@outlook.in>
2021-08-28 23:18:07 +0530

    write insert(), delete()

--- main.c
+++ main.c
@@ -18,31 +18,44 @@ void createArr()
         printf("Sucessfully created array.\n");
 }
 
-void scanA(int arr[], int size)
+void scanA(int arr[])
 {
-    for (int i = 0; i < size; i++)                          // taking input in the array
+    for (int i = 0; i < sizeArr; i++)                          // taking input in the array
     {
         printf("Enter Element for position %d : ", i);
         scanf("%d", &arr[i]);
     }
 }
 
-void printA(int arr[], int size)
+void printA(int arr[])
 {
     printf("[ %d", arr[0]);
-    for (int i = 1; i < size; i++)                          
+    for (int i = 1; i < sizeArr; i++)                          
     {
         printf(", %d", arr[i]);
     }
     printf(" ] \n");
-    arr[0] = 42;
+}
+
+void insert(int arr[], int x){
+    A = (int*)realloc(A, ++sizeArr*sizeof(int) );
+    A[sizeArr - 1 ] = x;
+}
+
+void delete(int arr[]){
+    A = (int*)realloc(A, --sizeArr*sizeof(int) );
 }
 
 int main(int argc, char *argv[])                            // Driver code
 {   
     int n;
     createArr();
-    scanA(A, sizeArr);
-    printA(A, sizeArr);
+    scanA(A);
+    printA(A);
+    insert(A, 42);
+    printA(A);
+    delete(A);
+    printA(A);
+
     
 }
